b55a3247f1dcfea93ef4faf46e114e5be065fa10
Patrick Bauer <patrick.jean.bauer@googlemail.com>
2014-02-03 00:10:57 +0100

    Titel + XML Export

--- src/HTW/PhotoWebBundle/Controller/PhotoController.php
+++ src/HTW/PhotoWebBundle/Controller/PhotoController.php
@@ -18,39 +18,67 @@ class PhotoController extends Controller
 {
 
 	/**
-     * @Route("/upload")
-     */
-    public function uploadAction(Request $request)
-    {
-    	$user = $this->getUser()->getId();
-	    $photo = new Photo($user);
-	    $form = $this->createFormBuilder($photo)
-	        ->add('name')
-	        ->add('description')
-	        ->add('file')
-	        ->add('save', 'submit', array('attr'=> array('class'=>'test')))
-	        ->add('saveAndAdd', 'submit')
-	        ->getForm();
+	 * @Route("/upload")
+	 */
+	public function uploadAction(Request $request)
+	{
+		$user = $this->getUser()->getId();
+		$photo = new Photo($user);
+		$form = $this->createFormBuilder($photo)
+			->add('name', 'text', array('label' => 'Name'))
+			->add('description', 'textarea', array('label' => 'Beschreibung'))
+			->add('file', 'file', array('label' => 'Datei'))
+			->add('save', 'submit', array('label'=> 'Hinzufügen'))
+			->add('saveAndAdd', 'submit', array('label'=> 'Hinzufügen und weiteres Foto anlegen'))
+			->getForm();
 
-	    $form->handleRequest($request);
+		$form->handleRequest($request);
 
-    	if ($form->isValid()) {
-		    $em = $this->getDoctrine()->getManager();
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
 
-		    $em->persist($photo);
-		    $em->flush();
+			$em->persist($photo);
+			$em->flush();
 
 
-		    $this->get('session')->getFlashBag()->add('notice', 'Your image was uploaded successfully'); 
+			$this->get('session')->getFlashBag()->add('notice', 'Your image was uploaded successfully'); 
 
-		    // Stay on the upload page if second button is clicked
-		    if($form->get('saveAndAdd')->isClicked()) {
-		    	return $this->redirect($this->generateUrl('htw_photoweb_photo_upload'));
-		    }
+			// Stay on the upload page if second button is clicked
+			if($form->get('saveAndAdd')->isClicked()) {
+				return $this->redirect($this->generateUrl('htw_photoweb_photo_upload'));
+			}
 
-		    return $this->redirect($this->generateUrl('htw_photoweb_myarea_index'));
+			return $this->redirect($this->generateUrl('htw_photoweb_myarea_index'));
 		}
 
 		return $this->render('HTWPhotoWebBundle:Photo:upload.html.twig', array('form' => $form->createView()));
-    }
+	}
+
+	/**
+	 * @Route("/export")
+	 * @Method("GET")
+	 */
+	public function exportAction()
+	{
+		$userID = $this->getUser()->getId();
+
+		$repository = $this->getDoctrine()
+						   ->getRepository('HTWPhotoWebBundle:Photo');
+
+		$query = $repository->createQueryBuilder('p')
+							->where('p.user = :userID')
+							->setParameter('userID', $userID)
+							->getQuery();
+
+		$templating = $this->get('templating');
+		$content = $templating->render(
+		    'HTWPhotoWebBundle:Photo:export.html.twig',
+		    array('photos' => $query->getResult())
+		);
+
+		$response = new Response($content);
+		$response->headers->set('Content-Type', 'application/xml');
+
+		return $response;
+	}
 }
