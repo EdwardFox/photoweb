2c9d3b29cdb5a7d9a9e129326f0ee5e97826b951
Patrick Bauer <patrick.jean.bauer@googlemail.com>
2014-02-02 22:32:48 +0100

    Kontaktformular

--- src/HTW/PhotoWebBundle/Controller/StaticPageController.php
+++ src/HTW/PhotoWebBundle/Controller/StaticPageController.php
@@ -11,7 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
-
+use Symfony\Component\Validator\Constraints as Assert;
 
 class StaticPageController extends Controller
 {
@@ -31,5 +31,54 @@ class StaticPageController extends Controller
      * @Route("/contact")
      * @Template("HTWPhotoWebBundle:StaticPage:contact.html.twig")
      */
-    public function contactAction() { return array(); }
+    public function contactAction(Request $request) { 
+        
+        $form = $this->createFormBuilder()
+            ->add('name', 'text', array(
+                'label' => 'Ihr Name',
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                )
+            ))
+            ->add('email', 'email', array(
+                'label' => 'Ihre E-Mail Adresse',
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                    new Assert\Email(array(
+                        'message' => 'Die Adresse "{{ value }}" ist keine valide E-Mail Adresse.',
+                        'checkMX' => false,
+                    ))
+                )
+            ))
+            ->add('message', 'textarea', array(
+                'label' => 'Ihre Nachricht an uns',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
+            ))
+            ->add('send', 'submit', array('label' => 'Nachricht abschicken'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Neue Nachricht von '. $data['name'] .' über Fotoweb!')
+                ->setFrom($data['email'])
+                ->setTo('patrick.jean.bauer@googlemail.com')
+                ->setBody($data['message']);
+            $this->get('mailer')->send($message);
+
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                'Vielen Dank, Ihre Nachricht wurde entgegen genommen.'
+            );
+
+            return $this->redirect($this->generateUrl('htw_photoweb_staticpage_contact'));
+        }
+
+        return array('form' => $form->createView()); 
+    }
 }
